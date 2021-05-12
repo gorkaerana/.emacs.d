7f96471f9b021146bb9c39a630e73bf3f00d6e03
gorkaerana <gorka.e.r@gmail.com>
2021-05-12 22:28:07 +0200

    Deleted directories that were mistakenly tracked

--- elpa/laguna-theme-20200928.2159/laguna-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "laguna-theme" "20200928.2159" "A theme that's easy on the eyes & focuses on importance." 'nil :commit "61b18f6362b94e42ea5ab19a6f2debc2bd917eda" :authors '(("Henry Newcomer" . "a.cliche.email@gmail.com")) :maintainer '("Henry Newcomer" . "a.cliche.email@gmail.com") :url "https://github.com/HenryNewcomer/laguna-theme")
