401ad3cd14c7bb94de9077a2f87b3d124a73df1f
Gorka Eraña <eranagorka@gmail.com>
2022-07-14 11:06:26 +0200

    Update init.el

--- init.el
+++ init.el
@@ -19,6 +19,10 @@
   :config (load-theme 'material t))
 
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((t (:height 160 :family "Hack")))))
 
 (use-package rainbow-delimiters
@@ -224,3 +228,10 @@
 (prefer-coding-system 'utf-8)
 
 (use-package org-make-toc :ensure t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(julia-mode yaml-mode vterm use-package rainbow-delimiters py-autopep8 org-roam org-make-toc org-bullets neotree material-theme magit flycheck elpy dap-mode clojure-mode blacken)))
